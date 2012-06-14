0d2c34178535e843db54b73fa12f10fc73b24655
Harshad <hbehere@zeomega.com>
2012-06-14 15:51:45 +0530

    Modifications made to amend test for clean up

--- svc/tests/test_repository.py
+++ svc/tests/test_repository.py
@@ -1,11 +1,16 @@
 from ..repository import Repository
 
 import os
+import shutil
 
 def test_create_repository():
     """
-    Creates and initilizes repository
+    Test to create and initilize repository
     """
     test_repository = Repository("D:\\test_svc")
-    assert test_repository.storage_path == "D:\\test_svc\\.svcs"
-    assert os.path.exists(test_repository.storage_path)
+    storage_path = test_repository.storage_path
+    assert storage_path == "D:\\test_svc\\.svcs"
+    assert os.path.exists(storage_path)
+
+    # Delete test repository and storage
+    shutil.rmtree(storage_path)
