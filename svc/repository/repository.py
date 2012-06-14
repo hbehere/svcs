f609604fc0c0ca10d7432d87aff23f5cf5928baf
Harshad <hbehere@zeomega.com>
2012-06-14 17:40:27 +0530

    Modifications made to comlplete test case for repository commit and fix reposiotry __init__ issue

--- svc/repository/repository.py
+++ svc/repository/repository.py
@@ -2,18 +2,66 @@
 
 import os
 
+from ..storage import FileStorage, File, Commit
+
 class Repository(object):
     """
     Class for svc reposiotory
     """
     def __init__(self, location):
         """
+        @param : location - string - repository path
+
         Initializes repository.
 
         Creates .svcs storage directory for given repository path.
+        Also Initializes file system storage
         """
-        storage_path = location + os.sep + ".svcs"
-        if not os.path.exists(storage_path):
-            os.makedirs(storage_path)
+        if not os.path.exists(location):
+            os.makedirs(location)
 
-        self.storage_path = storage_path
+        storage_path = location + os.sep + ".svcs"
+
+        self.storage = FileStorage(storage_path)
+
+    def commit(self, msg, committer, date, files):
+        """
+        @param : msg - string - commit messgae
+        @param : committer - string - email of commiter
+        @param : date - datetime - date/time detail for commit
+        @param : files - list - list of file paths to be commited
+
+        Stores files and creates commit.
+        """
+        file_ids = []
+
+        # First store file objects
+        # We need to get file contains by reading file
+        for eachFile in files:
+            file_object = open(eachFile,"rb")
+            file_contents = file_object.read()
+            file_object.close()
+
+            # Store file for svc
+            s_file = File(file_contents)
+            s_file_id = s_file.id
+
+            # Store file object
+            self.storage.store_object(s_file)
+
+            # Update file ids list
+            file_ids.append((eachFile, s_file_id))
+
+        # Create commit object
+        s_c = Commit(committer=committer,
+                     message=msg,
+                     date=date,
+                     parent=None,
+                     files=file_ids,
+                     )
+
+        # Get commit id
+        commit_id = s_c.id
+
+        # Return commit id
+        return commit_id
--- svc/tests/test_repository.py
+++ svc/tests/test_repository.py
@@ -5,13 +5,12 @@ import datetime
 
 def test_create_repository(repository_path):
     """
-    Test to create and initilize repository
+    Test to create and initialize repository
     """
     test_repository = Repository(repository_path)
 
-    storage_path = test_repository.storage_path
+    storage_path = os.path.join(repository_path,".svcs")
 
-    assert storage_path ==  repository_path + os.sep + ".svcs"
     assert os.path.exists(storage_path)
 
 def test_commit(repository_path):
@@ -22,6 +21,20 @@ def test_commit(repository_path):
     c_msg = "Sample commit"
     c_commiter = "pytest@test.org"
     current_date = datetime.datetime.now()
-    test_repository.commit(msg=c_msg,
-                           commiter=c_commiter,
-                           date=current_date)
+
+    sample_file_path = repository_path + os.sep + "sample.txt"
+    sample_file = file(sample_file_path,"ab")
+    sample_file.write("This is sample text file")
+    sample_file.close()
+
+    # List of files to be commited - list contains path to file
+    files_list = [sample_file_path]
+
+    commit_id = test_repository.commit(msg=c_msg,
+                                       committer=c_commiter,
+                                       date=current_date,
+                                       files=files_list)
+
+    commit_path = os.path.join(repository_path, ".svcs", "objects", str(commit_id))
+
+    os.path.exists(commit_path)
