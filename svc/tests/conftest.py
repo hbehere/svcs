a14f9a9144f25cd485e080d0154eabb96661ee6a
Harshad <hbehere@zeomega.com>
2012-06-14 16:03:34 +0530

    Modifications made to use automatic tear down test cases

--- svc/tests/conftest.py
+++ svc/tests/conftest.py
@@ -7,5 +7,10 @@ def pytest_funcarg__file_store(request):
     s = FileStorage(store_dir)
     request.addfinalizer(lambda : shutil.rmtree(store_dir))
     return s
+
+def pytest_funcarg__repository_path(request):
+    repository_dir = "D:\\test_svc"
+    request.addfinalizer(lambda : shutil.rmtree(repository_dir))
+    return repository_dir
     
     
--- svc/tests/test_repository.py
+++ svc/tests/test_repository.py
@@ -3,17 +3,13 @@ from ..repository import Repository
 import os
 import shutil
 
-def test_create_repository():
+def test_create_repository(repository_path):
     """
     Test to create and initilize repository
     """
-    repository_path = "D:\\test_svc"
     test_repository = Repository(repository_path)
 
     storage_path = test_repository.storage_path
 
-    assert storage_path == "D:\\test_svc\\.svcs"
+    assert storage_path ==  repository_path + os.sep + ".svcs"
     assert os.path.exists(storage_path)
-
-    # Delete test repository and storage
-    shutil.rmtree(repository_path)
