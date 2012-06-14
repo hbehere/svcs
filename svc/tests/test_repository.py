7369dbc045052325d91c0ef773813d0a10181bf5
Harshad <hbehere@zeomega.com>
2012-06-14 15:13:56 +0530

    Modifications made to -- enhance test case for repository initialize test

--- svc/tests/test_repository.py
+++ svc/tests/test_repository.py
@@ -1,7 +1,10 @@
 from ..repository import Repository
 
+import os
+
 def test_create_repository():
     """
     Creates and initilizes repository
     """
     test_repository = Repository()
+    assert os.path.exists(test_repository.location)
